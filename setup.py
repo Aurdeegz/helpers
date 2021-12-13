17aed7b72c33825409975b0de5e99704b4821625
Kendeegz <kennydeegz@gmail.com>
2021-12-12 20:51:47 -0500

    Updates

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
+import setuptools
 
-setup(name = "helpers",
-      version = "0.1",
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name = "helpers-kendeegz",
+      version = "0.0.1",
       desrciption = "Helpers for statistics and data management.",
       url = "https://github.com/kendeegz/helpers",
       author = "Kenneth P. Callahan",
       author_email = "kennydeegz@gmail.com",
       license = "MIT",
-      packages = ["helpers"],
+      packages = setuptools.find_packages(where = "src"),
+      python_requires=">=3.8",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
       zipsafe = False)
